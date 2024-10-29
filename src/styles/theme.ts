c0776024db4393e043e10789e703572d7fe22026
yunse0708 <yunse070708@gmail.com>
2024-10-29 22:38:59 +0900

    initial commit

---
+++ src/styles/theme.ts
@@ -0,0 +1,18 @@
+const theme = {
+  gray100: "#F8F9FA",
+  gray200: "#F2F5F7",
+  gray300: "#DEE2E6",
+  gray400: "#CED4DA",
+  gray500: "#ADB5BD",
+  gray600: "#6B7280",
+  gray700: "#495057",
+  gray800: "#343A40",
+
+  orange200: "#FF7637",
+  orange300: "#FE4E26",
+
+  background: "#F5F5F5",
+  white: "#FFFFFF",
+};
+
+export default theme;
