0664fd788d26e1ecc64db00abe09f9d2d128c260
8954sood <8954sood@naver.com>
2024-10-30 10:12:33 +0900

    feat: Create Recipe Page View

---
+++ src/components/recipe/recipeIconItem/style.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const RecipeIconItemContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 4px;
+`
+
+export const RecipeIconItemIcon = styled.img`
+  width: 48px;
+  height: 48px;
+`
+
+export const RecipeIconItemText = styled.div`
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 1.3;
+  color: #000000;
+`
